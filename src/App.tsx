56abb1d4dc838eaf5f36b4f664995316b811e5b1
akutsang <akuutsang@gmail.com>
2023-10-27 02:10:59 +0100

    creat comonents

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { type } from "@testing-library/user-event/dist/type";
+import CryptoSummary from "./components/CryptoSummary";
 
 export type Crypto = {
 ath: number
@@ -27,7 +28,7 @@ function App() {
     }) 
   }, []);
   return <div className="App">{cryptos ? cryptos.map((crypto)=>{
-    return <p>{  crypto.name + ' $' + crypto.current_price}</p>
+    return <CryptoSummary crypto={crypto}/>
   }) : null}</div>;
 }
 
