e54cebab6a79612c51c74412e1885ba903e19d4c
akutsang <akuutsang@gmail.com>
2023-10-30 01:22:23 +0100

    set amount to number instead of string

--- src/components/CryptoSummary.tsx
+++ src/components/CryptoSummary.tsx
@@ -9,15 +9,15 @@ export type AppProps = {
 export default function CryptoSummary({crypto}: AppProps): JSX.Element{
 
     useEffect(()=>{
-        console.log(crypto.name, amount, crypto.current_price * parseFloat(amount))
+        console.log(crypto.name, amount, crypto.current_price * amount)
     })
 
-    const [amount, setAmount] = useState<string>('2')
+    const [amount, setAmount] = useState<number>(0)
     return (
         <div>
          {<span>{  crypto.name + ' $' + crypto.current_price}</span>}
-         <input type='number' style={{margin:10}} value={amount} onChange={(e)=>{setAmount(e.target.value)}}></input>
-         <p>${(crypto.current_price * parseFloat(amount)).toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2})}</p>
+         <input type='number' style={{margin:10}} value={amount} onChange={(e)=>{setAmount(parseFloat(e.target.value))}}></input>
+         <p>${(crypto.current_price * amount).toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2})}</p>
          </div>
          );
 
