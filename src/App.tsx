5f4e6d6245ed3e4d7b51775256f10f4f37e3225d
akutsang <akuutsang@gmail.com>
2023-10-31 01:18:11 +0100

    add portfolio calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import moment  from "moment";
 import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./types/Crypto";
 import {
@@ -14,8 +13,6 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import type { ChartData, ChartOptions } from 'chart.js';
 
 ChartJS.register(
   CategoryScale,
@@ -30,81 +27,36 @@ ChartJS.register(
 function App() {
 const [cryptos, setCryptos] = useState<Crypto[] | null>();
 const [selected, setSelected] = useState<Crypto []>([]);
-const [range, setRange] = useState<number>(30);
-// const [data, setData] = useState<ChartData<'line'>>();
-// const [options, setOptions] = useState<ChartOptions<'line'>>({
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       display: false,
-//     },
-//     title: {
-//       display: true,
-//       text: 'Chart.js Line Chart',
-//     },
-//   },
-// })
 
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
     axios.get(url).then((response) => {
-      //  return response.data
       setCryptos(response.data);
     });
   }, []);
 
-  // useEffect(()=>{
-  //   if (!selected) return
-  //   axios.get(
-  //     `https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${range === 1 ? 'interrval=hourly' : 'interval=daily'}`
-  //   )
-  //   .then((response)=>{
-  //     console.log(response.data)
-  //     setData(
-  //       {
-  //         labels : response.data.prices.map((price: number[])=>{
-  //       return moment(price[0] / 1000).format(range === 1 ? "HH:MM" : "MM-DD");
-  //         } 
-  //         ),
-  //         datasets: [
-  //           {
-  //             label: 'Dataset 1',
-  //             data: response.data.prices.map((price: number[])=>{
-  //               return price[1]} ) ,
-  //             borderColor: 'rgb(255, 99, 132)',
-  //             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  //           },
-          
-  //         ],
-  //       })
-  //       setOptions({
-          
-  //           responsive: true,
-  //           plugins: {
-  //             legend: {
-  //               display: false,
-  //             },
-  //             title: {
-  //               display: true,
-  //               text: `${selected?.name } Price Over Last `  +  range  + (range === 1 ? ' Day.' : ' Days.') ,
-  //             },
-  //           },
-  //         })
-        
-  //   });
+  useEffect(() => {
+console.log("SELECTED",selected)
+  }, [selected])
 
-  // }, [selected, range]);
+  function updateOwned(crypto:Crypto, amount:number){
+    console.log("updateowned", crypto, amount);
+    let temp = [...selected];
+    let tempObj = temp.find((c)=> c.id === crypto.id)
+    if (tempObj) {
+      tempObj.owned = amount;
+      setSelected(temp)
+     }
+  }
 
   return(
   <>
   <div className="App">
-    <select 
+    <select title="Select a crypto coin" name="category"
       onChange={(e)=>{
         const c = cryptos?.find((x)=> x.id === e.target.value) as Crypto;
         setSelected([...selected, c])
-        // when we select an option, we will;
-        // request
         
       }}
       defaultValue="default"
@@ -117,25 +69,22 @@ const [range, setRange] = useState<number>(30);
               <option value="default">Choose an option</option>
 
     </select>
-     {/* <select onChange={(e)=>{
-           setRange(parseInt(e.target.value))
-
-    }}>
-      <option value={30}>30days</option>
-      <option value={7}>7days</option>
-      <option value={1}>day</option>
-    </select> */}
-     
 
   </div>
   
    {selected.map((s)=>{
-   return <CryptoSummary crypto={s}/>
+   return <CryptoSummary crypto={s} updateOwned={updateOwned}/>
  })}
+{selected? "Your portfolio is worth $" + selected.map((s)=>{
+  if (isNaN(s.owned)) {
+    return 0
+    
+  }
+return s.current_price * s.owned;
+}).reduce((previous, current)=>{
+  return previous + current;
 
-   {/* { selected ? <CryptoSummary crypto={selected} /> : null }
-  {data ? <div style={{width: 600}}><Line options={options} data={data} /></div> : null}
-   */}
+}, 0).toLocaleString(undefined, {minimumFractionDigits:2, maximumFractionDigits:2}) : null}
 </>
   );
 }
--- src/components/CryptoSummary.tsx
+++ src/components/CryptoSummary.tsx
@@ -3,10 +3,12 @@ import { Crypto  }  from "../types/Crypto"
 
 
 export type AppProps = {
-    crypto: Crypto 
+    crypto: Crypto;
+
+    updateOwned: (crypto: Crypto, amount: number) => void;
 };
 
-export default function CryptoSummary({crypto}: AppProps): JSX.Element{
+export default function CryptoSummary({crypto, updateOwned}: AppProps): JSX.Element{
 
     useEffect(()=>{
         console.log(crypto.name, amount, crypto.current_price * amount)
@@ -16,8 +18,23 @@ export default function CryptoSummary({crypto}: AppProps): JSX.Element{
     return (
         <div>
          {<span>{  crypto.name + ' $' + crypto.current_price}</span>}
-         <input type='number' style={{margin:10}} value={amount} onChange={(e)=>{setAmount(parseFloat(e.target.value))}}></input>
-         <p>${(crypto.current_price * amount).toLocaleString(undefined,{minimumFractionDigits:2,maximumFractionDigits:2})}</p>
+         <input 
+         type='number' 
+         style={{margin:10}} 
+         value={amount} 
+         onChange={(e)=>{
+            setAmount(parseFloat(e.target.value));
+            updateOwned(crypto, parseFloat(e.target.value))
+            // set the parent state by calling a function passed in as a prop
+            }}
+            ></input>
+            <p>
+               {isNaN(amount) ? "$0.00": "$" +
+                (crypto.current_price * amount).toLocaleString(undefined,{
+                    minimumFractionDigits:2,
+                    maximumFractionDigits:2
+                })}
+            </p>
          </div>
          );
 
